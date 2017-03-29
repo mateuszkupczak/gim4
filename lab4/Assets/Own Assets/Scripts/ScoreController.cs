64d8d69f0129b97d0c7675e8597bec1aa8e91a0d
Mateusz Kupczak <mateusz49@o2.pl>
2017-03-29 23:47:29 +0200

    GiM - lab. nr 4

---
+++ lab4/Assets/Own Assets/Scripts/ScoreController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    public Text scoreText;
+
+    int score;
+
+    // Use this for initialization
+    void Start()
+    {
+        RefreshCountText();
+    }
+    
+    // When the player collides with any known target (tag)
+    void OnTriggerEnter(Collider other)
+    {
+        var encounteredObject = other.gameObject;
+        if (encounteredObject.CompareTag("Target") && encounteredObject.GetComponent<ConstantForce>().torque == new Vector3(20, 0, 0))
+        {
+            encounteredObject.GetComponent<ConstantForce>().torque = new Vector3(40, 0, 0);
+            HandleObjectCollision(other.gameObject, 1);
+        }
+    }
+
+    // Update the score based on the main criteria of how much we should add
+    private void HandleObjectCollision(GameObject currentGameObject, short scoreToAdd)
+    {
+        score += scoreToAdd;
+        RefreshCountText();
+    }
+
+    // Reproduce the score text.
+    private void RefreshCountText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
---
+++ lab4/Assets/Own Assets/Scripts/TriggerController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerController : MonoBehaviour
+{
+    // Handle some proper animation when the player touches it.
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Collider>().CompareTag("Player") && GetComponent<ConstantForce>().torque == new Vector3(0, 0, 0))
+        {
+            GetComponent<ConstantForce>().torque = new Vector3(20, 0, 0);
+        }
+    }
+}
